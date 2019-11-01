573a6b91cec6b99027ca4dba78b0d76e42956a1e
H <christopher@zabo.com>
2019-11-01 13:01:11 -0700

    update-for-sfbw

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,30 +2,58 @@ import './../styles/index.scss'
 import 'zabo-sdk-js'
 
 Zabo.init({
-  clientId: 'J28kJ68mHfiXSYLqyoujgFA4mCrRGxBiUkELZS1YImTyD4DAhNmR9VZqpiFpqRUV',
+  clientId: 'njACEbH5kqNEzaDBIP1orCcWth95okgdLRKXLnfBFsib3OK51mrvOoNKjkBEJohY',
   env: 'sandbox'
 })
-let div = document.createElement('div')
+
+let selectedClub = ''
+let toAddress = ''
 
 document.querySelector('#connect').addEventListener('click', () => {
   Zabo.connect().onConnection(function (account) {
     console.log('account connected:', account)
-    let joinText = document.getElementsByClassName('join')
-    for (let i = 0; i < joinText.length; i++) {
-      joinText[i].style.display = 'none'
-    }
-    let node = document.createTextNode("Congratulations, we see that you hold " + account.currencies.length + " currencies in this wallet. You are now part of the club. You may also join any division highlighted in green. You cannot join divisions highlighted in red.")
+
+    let node = document.createTextNode(
+      "Congratulations, we see that you hold " +
+      account.currencies.length +
+      " currencies in this wallet. You are now part of the Awesome Crypto Club.")
+
+    let div = document.createElement('div')
     div.appendChild(node)
-    document.querySelector('#club-info').appendChild(div)
+    document.querySelector('#member-welcome').appendChild(div)
+
     let clubButtons = document.getElementsByClassName('club')
     for (let i = 0; i < clubButtons.length; i++) {
       if (account.currencies.some(a => a.currency === clubButtons[i].id)) {
         clubButtons[i].style.color = 'rgb(39, 155, 54)'
         clubButtons[i].style.borderColor = 'rgb(39, 155, 54)'
+        clubButtons[i].addEventListener('click', () => {
+          selectedClub = clubButtons[i].id
+          toAddress = clubButtons[i].getAttribute('address')
+          document.getElementById('donation-input').style.display = 'block'
+        })
       }
     }
-    document.querySelector('#club-info').style.display = 'block'
   }).onError(error => {
     console.error('account connection error:', error)
   });
+});
+
+document.querySelector('#donate-button').addEventListener('click', () => {
+
+  let donateValue = document.getElementById('amount').value
+  Zabo.transactions.send({
+    currency: selectedClub,
+    toAddress: toAddress,
+    amount: donateValue
+  }).then(tx => {
+    let txInfoDiv = document.getElementById('tx-info')
+    let node = document.createTextNode(JSON.stringify(tx))
+    txInfoDiv.appendChild(node)
+    let bottomText = document.getElementsByClassName('bottom-text')
+    for (let i = 0; i < bottomText.length; i++) {
+      bottomText[i].style.display = 'block'
+
+    }
+  })
 });
